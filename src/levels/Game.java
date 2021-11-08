105fcf24ce0c95c6b90547dff9a8cad59d9a5f2b
Victor Henrique Moreira <Slipvic667@gmail.com>
2021-11-08 17:46:34 -0300

    Adição do desafio 01 e 02

--- src/levels/Game.java
+++ src/levels/Game.java
@@ -71,6 +71,95 @@ public class Game {
 			System.out.println("Obrigado, até a proxima.");
 		}
 		else {
+			//INICIO DO DESAFIO 1
+			texto = " -     DESAFIO 01     - ";
+
+			delay(texto);
+			texto = "Após desastres no Afeganistão e o Talibã tomar o poder do país, foi designado um agente da CIA para ir ao Afeganistão e ajudar na evacuação dos soldados americanos no aeroporto de Cabul no Afeganistão.\n"
+					+ "Chegando no aeroporto e após conseguir realizar a evacuação dos soldados o/a agente percebeu que estava faltando um grupo de soldados e sabendo que teria que dar uma explicação aos EUA sobre isso \nele então tem que decidir se vai a procura dos soldados perdidos ou não...";
+			delay(texto);
+			texto = " -     AÇÃO     - ";
+			delay(texto);
+			texto = "1 - Procurar soldados? ";
+			delay(texto);
+			texto = "2 - Não procurar soldados? ";
+			delay(texto);
+
+			int n1 = sc.nextInt();
+
+			switch (n1) {
+			case 1:
+				System.out.println(
+						" O/a agente fica em uma intensa procura sobre os soldados restantes até que um grupo de mulheres que foram salvas pelo grupo de soldados\n"
+								+ "fala que eles foram capturados pelo grupo terrorista do Talibã e que viu eles sendo levados dentro de um carro para fora do aeroporto, com isso em mente e sem poder deixar o local\n"
+								+ "o/a agente notifica seu general e espera uma resposta através de uma ligação do que continuar fazendo, ficar no aeroporto e conseguir evacuar os soldados e alguns civis ou deixar o aeroporto e ir atrás dos soldados capturado");
+				break;
+			case 2:
+				System.out.println(
+						" Com a recusa do agente em procurar os soldados perdidos a multidão que está no aeroporto percebe que sobra algumas vagas ainda no avião\n"
+								+ "e com isso gera uma grande briga entre os civis para ver quem vai entrar no avião e acaba que os civis tentam tomar controle do avião contra os soldados e isso gera uma briga intensa\n"
+								+ "entre soldados e civis, gerando até mesmo uma morte por conta da tentativa de tomada de avião, fazendo assim com que o/a agente tenha que verificar se nenhum repórter flagrou a morte do civil\n"
+								+ "e resolver o problema do homicídio e com isso notifica seu general através de um ligação e espera a resposta para saber o que fazer...");
+				break;
+			}
+			texto = " - Após a ação do jogador - ";
+			delay(texto);
+			texto = "Sendo a ação do jogador a (sim) ou a (não) ele espera da mesma forma o telefone de seu general, mas não esperava que quem vai ligar para ele é uma pessoa misteriosa, mas que sabe muito sobre ele.";
+			delay(texto);
+			texto = " - Fim do desafio 1 - ";
+			delay(texto);
+		}
+		// FINAL DESAFIO 1
+		
+		//INICIO DO DESAFIO 2
+		texto = " -     DESAFIO 02     - ";
+
+		delay(texto);
+		texto = "O agente atende à ligação que estava aguardando, surpreso, ele descobre que a pessoa que havia lhe ligado era um antigo pupilo o qual ele havia treinado uma década antes em seu tempo no Afeganistão.\n"
+				+ "Voltando no passado, esse rapaz chamado Hamid, integrava uma família que desprezava os EUA, mas ele não via problema na presença dos americanos no Afeganistão, e,\n"
+				+ "como o EUA oferecia alimentação e uma condição de vida melhor para quem era recrutado, ele optou por se tornar um recruta.\n"
+				+ "Conforme o passar do tempo, quase ao fim de seu treinamento, Hamid presenciou uma situação que mudou o seu destino.\n"
+				+ "\n"
+				+ "Em uma batida dos Estados Unidos em um vilarejo, eles suspeitaram que uma pessoa que tinha ligação com o grupo terrorista local a Isis-K, porém o rapaz conhecia aquela pessoa e a considerava alguém inocente.";
+		delay(texto);
+		texto = " -     AÇÃO     - ";
+		delay(texto);
+		texto = "1 - Ajudar o senhor? ";
+		delay(texto);
+		texto = "2 - Não ajudar o senhor? ";
+		delay(texto);
+
+		int n1 = sc.nextInt();
+
+		switch (n1) {
+		case 1:
+			System.out.println(
+					" Hamid toma atitude vendo aquela situação e tenta ajudar o senhor a qual estava sendo agredido, porém mesmo implorando para os soldados parassem com aquilo, eles continuaram até matar o homem.\n"
+							+ "Após todo o ocorrido os soldados americanos ocultaram a morte do senhor e mesmo com a população de testemunha do ocorrido, seus atos foram ignorados e ninguém acabou punido.\n"
+							+ "Ao testemunhar tudo aquilo, sua concepção a respeito dos Estados Unidos mudou, o garoto na qual não via problema algum na presença de americanos no Afeganistão, passou a odiá-los assim como seus familiares.");
+			break;
+		case 2:
+			System.out.println(
+					" Hamid permanece imóvel, quase em estado de choque testemunhando tudo aquilo sem saber o que fazer.\n"
+							+ "Vendo toda essa situação, imaginando como uma pessoa seria capaz de fazer tal ato a alguém inocente,\n"
+							+ "o rapaz pensa a respeito das suas decisões e passa a considerar os EUA invasores de seu país assim como seus familiares.");
+			break;
+		}
+		texto = " - Após a ação do jogador - ";
+		delay(texto);
+		texto = "Voltando para o presente, o agente atende a ligação e escuta uma voz calma e conhecida...\n"
+				+ "Hamid diz – “Lembra de mim “professor”?” sarcasticamente\n"
+				+ "Então o agente respondeu – “Quem é você?”\n"
+				+ "Hamid novamente diz – “Nossa, fico desapontado que você não lembra de mim, encontrei com seus amigos americanos hoje de manhã, nós saímos para dar um rolezinho por aí” diz novamente em um tom sarcástico.\n"
+				+ "O agente responde preocupado - “ONDE ESTÁ MINHA EQUIPE ??!!”\n"
+				+ "Hamid revela – “Tenho duas localizações para te passar, ainda tenho um pouco de esperança de que você seja uma pessoa boa então quero ver que atitude o senhor irá tomar professor”\n"
+				+ "“Tem um armazém localizado ao sul do aeroporto com uma fachada vermelha e branca, vá para lá se quiser encontrar os seus amigos, agora, caso você ainda seja a pessoa boa que eu costumava a conhecer,\n"
+				+ "um grupo de professoras estão presas em um prédio e chamas ao oeste de sua posição, apenas siga a fumaça. Agora, elas são pessoas inocentes, diferente dos seus amiguinhos americanos, que assassinaram um homem a sangue frio tempos atrás.”";
+		delay(texto);
+		texto = " - Fim do desafio 2 - ";
+		delay(texto);
+
+		// FINAL DO DESAFIO 2
 		
 		
 		//INICIO DO DESAFIO 4 
@@ -110,9 +199,10 @@ public class Game {
 		delay(texto);
 		
 		//FIM DO DESAFIO 4
-}	
 		sc.close();
-	}
+}	
+		
+	
 
 	static void delay(String TextoFuncao) {
 		int tamanhoDelay = TextoFuncao.length();
